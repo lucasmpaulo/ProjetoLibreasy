b5e85cabb625cf6f56b7a0de1d9569cfb27fa97a
Lucas Paulo <mesakilive@gmail.com>
2019-10-27 15:59:00 -0300

    Testando alteração

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -54,7 +54,7 @@
                         <div class="form-group row mb-0 mt-4">
                             <div class="col-md-10 offset-md-2 text-right">
                                 <button type="submit" class="btn btn-form">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Resetar Senha') }}
                                 </button>
                             </div>
                         </div>
