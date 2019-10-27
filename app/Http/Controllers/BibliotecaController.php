f4fd878d503d9a7bc37d1b579e9c02348494e5d6
Lucas Paulo <mesakilive@gmail.com>
2019-10-27 10:37:57 -0300

    Testando alteração

--- app/Http/Controllers/BibliotecaController.php
+++ app/Http/Controllers/BibliotecaController.php
@@ -82,7 +82,7 @@ class BibliotecaController extends Controller
         $verify = $biblioteca->user_id;
         $bibliotecas = Biblioteca::all();
         $livros = DB::table('livros')->select('livros.id', 'livros.isbn','livros.codigoLivro', 'livros.titulo', 'livros.biblioteca_id', 
-        'livros.numeroCopias', 'categorias.nome', 'livros.numPagina', DB::raw('CONCAT(autores.nome, autores.sobrenome) as nomeAutor'))
+        'livros.numeroCopias', 'categorias.nome', 'livros.numPagina', DB::raw('CONCAT(autores.nome, " ", autores.sobrenome) as nomeAutor'))
         ->join('bibliotecas', 'bibliotecas.id', '=', 'livros.biblioteca_id')
         ->join('autores', 'autores.id', '=', 'livros.autor_id')
         ->join('categorias', 'categorias.id', '=', 'livros.categoria_id')
@@ -102,7 +102,7 @@ class BibliotecaController extends Controller
         // $livros = $bibliotecas->livro()->paginate(2)->appends(request()->except('page'));
         $data = $request->all();
         $livros = DB::table('livros')->select('livros.id', 'livros.isbn','livros.codigoLivro', 'livros.titulo', 
-        'livros.numeroCopias', 'categorias.nome', 'livros.numPagina', DB::raw('CONCAT(autores.nome, autores.sobrenome) as nomeAutor'))
+        'livros.numeroCopias', 'categorias.nome', 'livros.numPagina', DB::raw('CONCAT(autores.nome, " ", autores.sobrenome) as nomeAutor'))
         ->join('bibliotecas', 'bibliotecas.id', '=', 'livros.biblioteca_id')
         ->join('autores', 'autores.id', '=', 'livros.autor_id')
         ->join('categorias', 'categorias.id', '=', 'livros.categoria_id')
